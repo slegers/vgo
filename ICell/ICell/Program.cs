7cf2e3179978e93f07eeb0ab84aa5379c8bd31a3
Yanice Slegers <yanice@wina.be>
2018-02-26 21:11:49 +0100

    ICell implemented

--- ICell/ICell/Program.cs
+++ ICell/ICell/Program.cs
@@ -1,15 +1,90 @@
-﻿using System;
+﻿using Solution;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ICell
-{
-    class Program
+namespace Solution { 
+    public interface ICell<T>
     {
-        static void Main(string[] args)
+         T Value
+         {
+            get;
+            set;
+        }
+        event Action<ICell<T>> ValueChanged;
+    }
+
+    public class Cell<T> : ICell<T>
+    {
+        private T value;
+
+        public event Action<ICell<T>> ValueChanged;
+
+        protected Cell(T initialValue)
         {
+            this.value = initialValue;
+        }
+
+        public virtual T Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                if (!AreEqual(this.value, value))
+                {
+                    this.value = value;
+                    NotifyObservers();
+                }
+            }
+        }
+
+        private bool AreEqual(T x, T y)
+        {
+            if (x == null)
+            {
+                return y == null;
+            }
+            else
+            {
+                return x.Equals(y);
+            }
+        }
+
+        private void NotifyObservers()
+        {
+            if (ValueChanged != null)
+            {
+                ValueChanged(this);
+            }
+        }
+    } 
+
+        public class Derived<T, R> : Cell<R>
+        {
+        private readonly Func<T, R> transformer;
+
+        protected Derived(ICell<T> cell, Func<T, R> transformer)
+                    : base(transformer(cell.Value))
+        {
+            this.transformer = transformer;
+            cell.ValueChanged += CellUpdated;
+        }
+        private void CellUpdated(ICell<T> cell)
+        {
+            base.Value = transformer(cell.Value);
+        }
+        public override R Value
+        {
+            get
+            {
+                return base.Value;
+            }
+            set
+            {
+                throw new InvalidOperationException();
+            }
         }
     }
 }
