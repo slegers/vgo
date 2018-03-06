5c629fe97bfc1fe796a40c84c807375f76182f0d
Yanice Slegers <yanice@wina.be>
2018-03-06 18:50:42 +0100

    implemented Binding and initialized Binding_grid

---
+++ Binding/Binding/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    public class Person  
+    {
+        private string _first, _last;
+        public Person(String first = "Tom", String last = "Ato")
+        {
+            this.Firstname = first;
+            this.Lastname = last;
+        }
+            public String Firstname
+        {
+            get
+            {
+                return _first;
+            }
+            set
+            {
+                _first = value;
+            }
+        }
+
+        public String Lastname
+        {
+            get
+            {
+                return this._last;
+            }
+            set
+            {
+                this._last = value;
+            }
+        }
+        
+    }
+}
